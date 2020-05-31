8791ed4be920f2e893e3a32df09862d9fde6a5a6
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-05-31 18:22:36 +0200

    Change angle from servo

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import time
 from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
+# Configuration
+
 kit.servo[0].set_pulse_width_range(600, 2400)
 kit.servo[0].actuation_range = 180
+
+# Run code
+
+time.sleep(1)
 kit.servo[0].angle = 90
+
+time.sleep(1)
+kit.servo[0].angle = 45
+
+time.sleep(1)
+kit.servo[0].angle = 135
