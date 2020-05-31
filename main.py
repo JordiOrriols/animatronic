9edd1e6a7e933ea110bbf6a11b724e6632899419
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-05-31 18:32:46 +0200

    Configure script

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ kit.servo[0].actuation_range = 180
 
 time.sleep(1)
 kit.servo[0].angle = 90
-
 time.sleep(1)
-kit.servo[0].angle = 45
 
-time.sleep(1)
-kit.servo[0].angle = 135
+deg = 90
+
+while deg != 0:
+    deg = int(input('Starting at ', deg, ' deg. Do you want to move to another position?\n'))
+    print('Moving to position ', deg, ' deg.')
+
+    time.sleep(1)
+
+    if deg < 0:
+        deg = 0
+    if deg > 180:
+        deg = 180
+
+    kit.servo[0].angle = deg
