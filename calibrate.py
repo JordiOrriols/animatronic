46fbf314a32c74b476fd5410cc3e9a35e8a091f2
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-05-31 19:11:28 +0200

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -1,30 +1,16 @@
 import time
 from adafruit_servokit import ServoKit
 
-kit = ServoKit(channels=16)
 
 # Configuration
 
-kit.servo[0].set_pulse_width_range(600, 2400)
-kit.servo[0].actuation_range = 180
+kit = ServoKit(channels=16)
 
-# Run code
-
-while 1:
-    servo = input('Select Servo:\n')
-    position = input('Select position in degrees?\n')
-    move_servo_to_angle(servo, position)
-
-# Helpers
-
-def move_servo_to_angle(servo, position):
-    print('Moving servo #', servo, 'to position ', position, ' deg.')
-
-    time.sleep(2)
-    servo = validate_servo(int(servo))
-    position = validate_position(int(position))
-
-    kit.servo[servo].angle = position
+i = 0
+while i < 15:
+    kit.servo[i].set_pulse_width_range(600, 2400)
+    kit.servo[i].actuation_range = 180
+    i += 1
 
 
 # Validators
@@ -52,4 +38,23 @@ def validate_position(position):
         position = 180
 
     return position
-    
+
+
+# Helpers
+
+def move_servo_to_angle(servo, position):
+    print('Moving servo #', servo, 'to position ', position, ' deg.')
+
+    time.sleep(2)
+    servo = validate_servo(int(servo))
+    position = validate_position(int(position))
+
+    kit.servo[servo].angle = position
+
+
+# Run code
+
+while 1:
+    servo = input('Select Servo:\n')
+    position = input('Select position in degrees?\n')
+    move_servo_to_angle(servo, position)
