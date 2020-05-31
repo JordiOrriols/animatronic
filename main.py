4d35285d62ae0140c0ea8a030eb4fe6784fa6379
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-05-31 23:35:20 +0200

    Try with point

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from adafruit_servokit import ServoKit
 
-from src.helpers import move_servo_to_angle, initialize_servos
+from .helpers import move_servo_to_angle, initialize_servos
 
 
 # Initialization
