89363d4c58532310a9a97fce98abb3abb012cf78
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-07 00:22:55 +0200

    Update import

--- common/servo.py
+++ common/servo.py
@@ -2,6 +2,7 @@
 import time
 
 from common.config import servos_data, fabric_servo_data
+from common.helpers import get_fabric_data
 from common.validators import validate_servo, validate_servo_position
 
 
