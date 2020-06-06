1917b23931a9f872b956aea7289d6eeb25f32e1b
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-07 00:20:28 +0200

    remove circular dependency

--- common/helpers.py
+++ common/helpers.py
@@ -1,7 +1,5 @@
 from common.config import servos_data, fabric_servo_data
-from common.validators import validate_servo, validate_servo_position
 
 def get_fabric_data(servo):
-    servo = validate_servo(servo)
     servo_type = servos_data[servo]['type']
     return fabric_servo_data[servo_type]
