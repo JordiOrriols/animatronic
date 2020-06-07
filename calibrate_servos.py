f21b085b49d485d33970c91e458a66f6dd120764
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-07 11:29:58 +0200

    Improve calibration script with + and -

--- calibrate_servos.py
+++ calibrate_servos.py
@@ -10,7 +10,22 @@ initialize_servos(kit)
 # Run code
 
 while 1:
-    print('\n\n\n', 'Next movement')
+    print('\n\n\n', 'Next adjustment')
     servo = input('Select Servo: ')
-    position = input('Select position in degrees: ')
+    position = input('Select start position in degrees: ')
     move_servo_to_angle(kit, int(servo), int(position))
+
+    
+    print( 'Type "+" or "-" to adjust the position. Press any other key to exit.', '\n')
+
+    while servo:
+        operation = input('Adjusting: ')
+        if operation == '+':
+            position = int(position) + 5
+        elif operation == '-':
+            position = int(position) - 5
+        else:
+            servo = None
+
+        if servo:
+            move_servo_to_angle(kit, int(servo), int(position))
