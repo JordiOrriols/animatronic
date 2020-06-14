f09abae5d395926b5d92791adfcbb1a7322d4dda
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-14 15:45:09 +0200

    encapsulate to func

--- calibrate_phonemes.py
+++ calibrate_phonemes.py
@@ -3,14 +3,18 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Calibrate Phonemes')
 window.geometry('500x300') 
- 
-l = tk.Label(window, bg='white', fg='black', width=20, text='empty')
-l.pack()
- 
-def print_selection(v):
-    l.config(text='you have selected ' + v)
 
-s = tk.Scale(window, label='try me', from_=0, to=10, length=200, showvalue=0, tickinterval=2, resolution=0.01, command=print_selection)
-s.pack()
+def show_servo_scale(servo: int):
+    l = tk.Label(window, bg='white', fg='black', width=20, text='Waiting')
+    l.pack()
+    
+    def print_selection(v):
+        l.config(text=v)
+
+    label = 'Servo ' + servo
+    s = tk.Scale(window, label=label, from_=0, to=10, length=200, showvalue=0, tickinterval=2, resolution=0.01, command=print_selection)
+    s.pack()
  
+show_servo_scale(0)
+
 window.mainloop()
