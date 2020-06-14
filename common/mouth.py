9703d853a1011f78738ed202f3f16ed0e8c997d8
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-14 14:44:34 +0200

    Validate phonemes

--- common/mouth.py
+++ common/mouth.py
@@ -2,6 +2,11 @@ from common.config import phonemes_data
 from common.servo import move_servo_to_angle
 
 def adopt_phoneme(kit, phoneme):
+
+    if phoneme not in phonemes_data.keys():
+        print('Not defined phoneme', phoneme)
+        return
+
     current_phoneme = phonemes_data[phoneme]
     print('Adopting phoneme', phoneme)
     for data in current_phoneme:
