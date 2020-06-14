93e9f994cfe1f9f9e27a42fc24f9f84731229b77
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-14 16:45:25 +0200

    Moving servos

--- calibrate_phonemes.py
+++ calibrate_phonemes.py
@@ -1,30 +1,40 @@
-import tkinter as tk
+from adafruit_servokit import ServoKit
+from tkinter import Tk, Frame, Scale, Label
+
+from common.servo import initialize_servos, move_servo_to_angle
 from common.config import servos_data
 from common.validators import validate_controllable_servo
 
-window = tk.Tk()
+# Initialization
+kit = ServoKit(channels=16)
+
+initialize_servos(kit)
+
+window = Tk()
 window.title('Calibrate Phonemes')
 window.geometry('1000x300')
 
+# Run code
 
 def show_servo_scale(servo: int, min: int, max: int):
 
-    frameControl = tk.Frame(window)
+    frameControl = Frame(window)
     frameControl.pack(side='left', padx=10)
     
     label = '#' + str(servo)
-    n = tk.Label(frameControl, fg='black', width=3, text=label)
+    n = Label(frameControl, fg='black', width=3, text=label)
     n.pack(side='top', expand=True)
 
     def print_selection(v):
         l.config(text=v)
+        move_servo_to_angle(kit, servo, v)
 
-    s = tk.Scale(frameControl, from_=min, to=max, length=200,
+    s = Scale(frameControl, from_=min, to=max, length=200,
                  showvalue=0, tickinterval=2, resolution=5, command=print_selection)
 
     s.pack(side='top', expand=True)
 
-    l = tk.Label(frameControl, bg='white', fg='black', width=3, text='...')
+    l = Label(frameControl, bg='white', fg='black', width=3, text='...')
     l.pack(side='top', expand=True)
 
 for i in range(len(servos_data)):
