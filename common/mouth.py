122ad669da06bbd36c6e1b687859a4824b44d09d
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-14 14:39:12 +0200

    Fix adopt mouth

--- common/mouth.py
+++ common/mouth.py
@@ -3,7 +3,7 @@ from common.servo import move_servo_to_angle
 
 def adopt_phoneme(kit, phoneme):
     current_phoneme = phonemes_data[phoneme]
-    print('\n\n\n', 'Adopting phoneme', phoneme)
+    print('Adopting phoneme', phoneme)
     for data in current_phoneme:
-        print('\n', 'Phoneme data', data)
-        move_servo_to_angle(kit, data.servo, data.angle)
+        print('Phoneme data', data)
+        move_servo_to_angle(kit, data['servo'], data['angle'])
