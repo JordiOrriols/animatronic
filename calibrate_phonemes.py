3d5a822b37c50aa8c702b3205f3f5d39966fed54
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2020-06-14 16:15:19 +0200

    Pack left

--- calibrate_phonemes.py
+++ calibrate_phonemes.py
@@ -1,27 +1,31 @@
 import tkinter as tk
 from common.config import servos_data
- 
+
 window = tk.Tk()
 window.title('Calibrate Phonemes')
-window.geometry('800x400') 
+window.geometry('800x400')
+
 
 def show_servo_scale(servo: int, min: int, max: int):
     l = tk.Label(window, bg='white', fg='black', width=3, text='...')
-    l.pack()
-    
+    l.pack(side='left', padx=5)
+
     def print_selection(v):
         l.config(text=v)
 
     label = 'S#' + str(servo)
-    s = tk.Scale(window, label=label, from_=min, to=max, length=200, showvalue=0, tickinterval=2, resolution=5, command=print_selection)
-    
-    s.pack(side = 'left', padx = 5)
+    s = tk.Scale(window, label=label, from_=min, to=max, length=200, threshold=90,
+                 showvalue=0, tickinterval=2, resolution=5, command=print_selection)
+
+    s.pack(side='left', padx=5)
+
 
 for i in range(len(servos_data)):
 
     if servos_data[i]['type'] == 'disabled':
         continue
 
-    show_servo_scale(i, servos_data[i]['physical_limits']['min'], servos_data[i]['physical_limits']['max'])
+    show_servo_scale(i, servos_data[i]['physical_limits']
+                     ['min'], servos_data[i]['physical_limits']['max'])
 
 window.mainloop()
