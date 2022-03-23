27f533beef4b837a1693af7aee8546a9ebca97f1
Jordi Orriols <info@jordiorriols.cat>
2022-03-23 20:47:50 +0100

    Use real names for servos config

--- mouth/config.py
+++ mouth/config.py
@@ -1,28 +1,28 @@
 # Configuration
 
-from common.config import big_type, blue_type
+from common.config import mg996r_type, mg92b_type
 
 servos_data = [
-    {'type': big_type, 'physical_limits': {'min': 70, 'max': 90},
+    {'type': mg996r_type, 'physical_limits': {'min': 70, 'max': 90},
         'connection': {'servo': 1, 'type': 'inverted'}},  # 0
-    {'type': big_type, 'physical_limits': {'min': 90, 'max': 110}},  # 1
+    {'type': mg996r_type, 'physical_limits': {'min': 90, 'max': 110}},  # 1
 
-    {'type': blue_type, 'physical_limits': {'min': 40, 'max': 155}},  # 2
-    {'type': blue_type, 'physical_limits': {'min': 30, 'max': 135}},  # 3
-    {'type': blue_type, 'physical_limits': {'min': 15, 'max': 125}},  # 4
-    {'type': blue_type, 'physical_limits': {'min': 30, 'max': 150}},  # 5
-    {'type': blue_type, 'physical_limits': {'min': 45, 'max': 110}},  # 6
-    {'type': blue_type, 'physical_limits': {'min': 85, 'max': 135}},  # 7
-    {'type': blue_type, 'physical_limits': {'min': 30, 'max': 95}},  # 8
+    {'type': mg92b_type, 'physical_limits': {'min': 40, 'max': 155}},  # 2
+    {'type': mg92b_type, 'physical_limits': {'min': 30, 'max': 135}},  # 3
+    {'type': mg92b_type, 'physical_limits': {'min': 15, 'max': 125}},  # 4
+    {'type': mg92b_type, 'physical_limits': {'min': 30, 'max': 150}},  # 5
+    {'type': mg92b_type, 'physical_limits': {'min': 45, 'max': 110}},  # 6
+    {'type': mg92b_type, 'physical_limits': {'min': 85, 'max': 135}},  # 7
+    {'type': mg92b_type, 'physical_limits': {'min': 30, 'max': 95}},  # 8
 
     {'type': 'disabled'},
 
-    {'type': blue_type, 'physical_limits': {'min': 0, 'max': 180}},  # 10
-    {'type': blue_type, 'physical_limits': {'min': 0, 'max': 180}},  # 11
-    {'type': blue_type, 'physical_limits': {'min': 0, 'max': 180}},  # 12
-    {'type': blue_type, 'physical_limits': {'min': 0, 'max': 180}},  # 13
-    {'type': blue_type, 'physical_limits': {'min': 0, 'max': 180}},  # 14
-    {'type': blue_type, 'physical_limits': {'min': 0, 'max': 180}},  # 15
+    {'type': mg92b_type, 'physical_limits': {'min': 0, 'max': 180}},  # 10
+    {'type': mg92b_type, 'physical_limits': {'min': 0, 'max': 180}},  # 11
+    {'type': mg92b_type, 'physical_limits': {'min': 0, 'max': 180}},  # 12
+    {'type': mg92b_type, 'physical_limits': {'min': 0, 'max': 180}},  # 13
+    {'type': mg92b_type, 'physical_limits': {'min': 0, 'max': 180}},  # 14
+    {'type': mg92b_type, 'physical_limits': {'min': 0, 'max': 180}},  # 15
 ]
 
 phonemes_data = {
