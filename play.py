3e28822b89ac6330b67842512198ec101a2ca0b7
Jordi Orriols <info@jordiorriols.cat>
2022-03-25 21:03:00 +0100

    Update play.py

--- play.py
+++ play.py
@@ -16,18 +16,27 @@ initialize_servos(kit, servos_data)
 with open('scheleton/animation.json') as json_file:
     data = json.load(json_file)
 
-    print('\n\n\n', 'Animation loaded')
+    print('Animation loaded')
 
     while 1:
-        print('\n\n\n', 'System Ready')
-        print('\n', 'Playing at ', data.fps, 'fps')
-        print('\n', data.frames, 'Frames')
+        print('\n\n', 'System Ready')
+        print('\n', 'Playing at ', data['fps'], 'fps')
+        print('\n', data['frames'], 'Frames')
         start = input('Press any key to start: ')
 
         current_frame = 0
+        fps = data['fps']
+        frames = data['frames']
+        positions = data['positions']
 
-        for servo in servos_data:
-            new_position = data.positions[servo.getName()][current_frame]
-            servo.move_to_angle(new_position)
-            time.sleep(1 / data.fps)
+        while current_frame < frames:
+            for servo in servos_data:
+                if servo.getName() in positions.keys():
+                    new_position = positions[servo.getName()][current_frame]
+                    servo.move_to_angle(int(new_position))
+                else:
+                    print('Servo not found on animation: ', servo.getName(), servo.getPin())
+            
+            time.sleep(1 / fps)
+            time.sleep(5)
 
