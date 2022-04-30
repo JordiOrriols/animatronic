754c06954b0e31ab964a7720db96198d3c252eb1
Jordi Orriols <info@jordiorriols.cat>
2022-04-30 17:41:43 +0200

    set new limits

--- scheleton/config.py
+++ scheleton/config.py
@@ -8,16 +8,16 @@ servos_data = [
 
     AniServo('body-rotation', 1, mg90s_type, 45, 145, 95),
     AniServo('head-rotation', 2, mg90s_type, 40, 155, 90),
-    AniServo('head-flexion', 3, mg90s_type, 0, 180, 90),
+    AniServo('head-flexion', 3, mg90s_type, 60, 140, 90),
     AniServo('shoulder-left-flexion', 4, mg90s_type, 10, 170, 140),
     AniServo('shoulder-right-flexion', 5, mg90s_type, 10, 170, 45),
-    AniServo('shoulder-left-rotation', 6, mg90s_type, 0, 180, 90), # Broken Servo
+    AniServo('shoulder-left-rotation', 6, mg90s_type, 5, 90, 90),
     AniServo('shoulder-right-rotation', 7, mg90s_type, 95, 180, 100),
 
     AniServo('arm-left-rotation', 8, ghs37a_type, 80, 150, 100),
     AniServo('arm-right-rotation', 9, ghs37a_type, 50, 130, 90),
-    AniServo('hand-left-rotation', 10, ghs37a_type, 0, 180, 90),
-    AniServo('hand-right-rotation', 11, ghs37a_type, 0, 180, 90),
+    AniServo('hand-left-rotation', 10, ghs37a_type, 0, 155, 90),
+    AniServo('hand-right-rotation', 11, ghs37a_type, 0, 155, 90),
 
-    AniServo('mandible', 12, ghs37a_type, 0, 180, 90)
+    AniServo('mandible', 12, ghs37a_type, 60, 110, 90)
 ]
