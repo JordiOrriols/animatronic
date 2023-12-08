5b61c70cf955010592699477efb456272758066f
Jordi Orriols <info@jordiorriols.cat>
2023-12-08 23:37:10 +0100

    Remove try catch

--- server.py
+++ server.py
@@ -4,21 +4,18 @@ from playsound import playsound
 from common.socket import url, port, messages
 
 async def handler(websocket):
-    try:
-        async for message in websocket:
+    async for message in websocket:
 
-            print(message)
-            await websocket.send(message)
+        print(message)
+        await websocket.send(message)
 
-            if message == messages.ready:
-                await websocket.send(messages.waiting)
-                input('Press any key to start')
-                await websocket.send(messages.play)
-                playsound('sound/background.mp3', False)
-                playsound('sound/laugh.mp3', False)
+        if message == messages.ready:
+            await websocket.send(messages.waiting)
+            input('Press any key to start')
+            await websocket.send(messages.play)
+            playsound('sound/background.mp3', False)
+            playsound('sound/laugh.mp3', False)
 
-    except websockets.ConnectionClosedOK:
-        print('Server Closed')
 
 async def main():
     async with serve(handler, url, port):
