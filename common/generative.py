054d04d169a4584ae430a40d088e842d91d35838
Jordi Orriols <info@jordiorriols.cat>
2023-12-10 11:01:15 +0100

    make that movement is handled automatically inside the class

--- common/generative.py
+++ common/generative.py
@@ -18,6 +18,10 @@ class AnimatronicController:
 
         initialize_servos(self.kit, [self.servo])
 
+        self.is_movement_in_progress = False
+        self.next_target_position = None
+        self.next_duration = None
+
     def generate_smooth_movement(self, target_position, duration):
         current_position = self.kit.servo[self.servo.getPin()].angle
         angle_change = (target_position - current_position) / self.steps
@@ -27,43 +31,47 @@ class AnimatronicController:
             self.servo.move_to_angle(int(current_position))
             time.sleep(duration / self.steps)
 
-    def generate_random_smooth_movement(self):
-        min_limit = self.servo.getPhysicalLimitMin()
-        max_limit = self.servo.getPhysicalLimitMax()
+    def perform_random_movement(self):
+        if not self.is_movement_in_progress:
+            # Si no hay un movimiento en curso, generar nuevos valores aleatorios
+            min_limit = self.servo.getPhysicalLimitMin()
+            max_limit = self.servo.getPhysicalLimitMax()
 
-        # Generar una posición aleatoria dentro de los límites físicos del servo
-        random_position = random.randint(min_limit, max_limit)
+            # Generar una posición aleatoria dentro de los límites físicos del servo
+            self.next_target_position = random.randint(min_limit, max_limit)
 
-        # Duración aleatoria para el movimiento
-        duration = random.uniform(self.min_duration, self.max_duration)
+            # Duración aleatoria para el movimiento
+            self.next_duration = random.uniform(self.min_duration, self.max_duration)
 
-        # Mover suavemente el servo a la posición aleatoria
-        self.generate_smooth_movement(random_position, duration)
+            # Actualizar el indicador de movimiento en curso
+            self.is_movement_in_progress = True
+
+        # Mover suavemente el servo a la posición almacenada
+        self.generate_smooth_movement(self.next_target_position, self.next_duration)
+
+        # Actualizar el indicador de movimiento en curso
+        self.is_movement_in_progress = False
 
 
 class RandomMovementsController:
     def __init__(self, servos_data):
         self.kit = ServoKit(channels=16)  # Asegúrate de ajustar el número de canales según tu configuración
-        self.servos_data = servos_data
         self.animatronic_controllers = [
-            AnimatronicController(servo) for servo in self.servos_data
+            AnimatronicController(servo) for servo in servos_data
         ]
 
-    def perform_random_smooth_movements(self, duration_between_movements=2.0):
-        while True:
-            # Realizar movimientos suaves aleatorios para cada servo
-            for controller in self.animatronic_controllers:
-                controller.generate_random_smooth_movement()
-
-            # Esperar un tiempo antes de realizar el próximo conjunto de movimientos
-            time.sleep(duration_between_movements)
+    def perform_all_movements(self):
+        for controller in self.animatronic_controllers:
+            controller.perform_random_movement()
 
 if __name__ == "__main__":
     # Crear una instancia de RandomMovementsController
     random_controller = RandomMovementsController(skeleton_servos_data)
 
     try:
-        # Iniciar movimientos suaves aleatorios para todos los servos
-        random_controller.perform_random_smooth_movements()
+        while True:
+            # Realizar todos los movimientos
+            random_controller.perform_all_movements()
+
     except KeyboardInterrupt:
         print("\nMovimientos aleatorios suaves detenidos.")
