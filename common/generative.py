64c2f7f5e25e3ff5d4d55c7928d7a2926ac57bc2
Jordi Orriols <info@jordiorriols.cat>
2023-12-10 10:31:36 +0100

    separate code in 2 classes

--- common/generative.py
+++ common/generative.py
@@ -9,27 +9,27 @@ from adafruit_servokit import ServoKit  # Asegúrate de tener instalada esta bib
 # ...
 
 class AnimatronicController:
-    def __init__(self, servos_data, max_duration=2.0, min_duration=0.5, steps=50):
+    def __init__(self, servo, max_duration=2.0, min_duration=0.5, steps=50):
         self.kit = ServoKit(channels=16)  # Asegúrate de ajustar el número de canales según tu configuración
-        self.servos_data = servos_data
+        self.servo = servo
         self.max_duration = max_duration
         self.min_duration = min_duration
         self.steps = steps
 
-        initialize_servos(self.kit, self.servos_data)
+        initialize_servos(self.kit, [self.servo])
 
-    def generate_smooth_movement(self, servo, target_position, duration):
-        current_position = servo.__kit.servo[servo.getPin()].angle
+    def generate_smooth_movement(self, target_position, duration):
+        current_position = self.kit.servo[self.servo.getPin()].angle
         angle_change = (target_position - current_position) / self.steps
 
         for _ in range(self.steps):
             current_position += angle_change
-            servo.move_to_angle(int(current_position))
+            self.servo.move_to_angle(int(current_position))
             time.sleep(duration / self.steps)
 
-    def generate_random_smooth_movement(self, servo):
-        min_limit = servo.getPhysicalLimitMin()
-        max_limit = servo.getPhysicalLimitMax()
+    def generate_random_smooth_movement(self):
+        min_limit = self.servo.getPhysicalLimitMin()
+        max_limit = self.servo.getPhysicalLimitMax()
 
         # Generar una posición aleatoria dentro de los límites físicos del servo
         random_position = random.randint(min_limit, max_limit)
@@ -38,23 +38,32 @@ class AnimatronicController:
         duration = random.uniform(self.min_duration, self.max_duration)
 
         # Mover suavemente el servo a la posición aleatoria
-        self.generate_smooth_movement(servo, random_position, duration)
+        self.generate_smooth_movement(random_position, duration)
 
-    def perform_random_smooth_movements(self):
+
+class RandomMovementsController:
+    def __init__(self, servos_data):
+        self.kit = ServoKit(channels=16)  # Asegúrate de ajustar el número de canales según tu configuración
+        self.servos_data = servos_data
+        self.animatronic_controllers = [
+            AnimatronicController(servo) for servo in self.servos_data
+        ]
+
+    def perform_random_smooth_movements(self, duration_between_movements=2.0):
         while True:
-            # Lista de servos seleccionados aleatoriamente para este movimiento
-            random_servos = random.sample(self.servos_data, k=random.randint(1, len(self.servos_data)))
+            # Realizar movimientos suaves aleatorios para cada servo
+            for controller in self.animatronic_controllers:
+                controller.generate_random_smooth_movement()
 
-            # Generar y aplicar movimientos suaves aleatorios para cada servo seleccionado
-            for random_servo in random_servos:
-                self.generate_random_smooth_movement(random_servo)
+            # Esperar un tiempo antes de realizar el próximo conjunto de movimientos
+            time.sleep(duration_between_movements)
 
 if __name__ == "__main__":
-    # Crear una instancia de AnimatronicController
-    animatronic_controller = AnimatronicController(skeleton_servos_data)
+    # Crear una instancia de RandomMovementsController
+    random_controller = RandomMovementsController(skeleton_servos_data)
 
     try:
-        # Iniciar movimientos suaves aleatorios
-        animatronic_controller.perform_random_smooth_movements()
+        # Iniciar movimientos suaves aleatorios para todos los servos
+        random_controller.perform_random_smooth_movements()
     except KeyboardInterrupt:
         print("\nMovimientos aleatorios suaves detenidos.")
