9fe5b10cba20c98aeb44c598ba649f9a9de87f0c
Jordi Orriols <info@jordiorriols.cat>
2023-12-10 10:15:21 +0100

    make it a class

--- common/generative.py
+++ common/generative.py
@@ -8,43 +8,48 @@ from adafruit_servokit import ServoKit  # Asegúrate de tener instalada esta bib
 # Aquí deberías tener definidas las clases AniServo, fabric_servo_data y initialize_servos
 # ...
 
-def generate_smooth_movement(servo, target_position, duration=1.0, steps=50):
-    current_position = servo.__kit.servo[servo.getPin()].angle
-    angle_change = (target_position - current_position) / steps
+class AnimatronicController:
+    def __init__(self, servos_data, duration=1.0, steps=50):
+        self.kit = ServoKit(channels=16)  # Asegúrate de ajustar el número de canales según tu configuración
+        self.servos_data = servos_data
+        self.duration = duration
+        self.steps = steps
 
-    for _ in range(steps):
-        current_position += angle_change
-        servo.move_to_angle(int(current_position))
-        time.sleep(duration / steps)
+        initialize_servos(self.kit, self.servos_data)
 
-def generate_random_smooth_movement(servo, duration=1.0, steps=50):
-    min_limit = servo.getPhysicalLimitMin()
-    max_limit = servo.getPhysicalLimitMax()
+    def generate_smooth_movement(self, servo, target_position):
+        current_position = servo.__kit.servo[servo.getPin()].angle
+        angle_change = (target_position - current_position) / self.steps
 
-    # Generar una posición aleatoria dentro de los límites físicos del servo
-    random_position = random.randint(min_limit, max_limit)
+        for _ in range(self.steps):
+            current_position += angle_change
+            servo.move_to_angle(int(current_position))
+            time.sleep(self.duration / self.steps)
 
-    # Mover suavemente el servo a la posición aleatoria
-    generate_smooth_movement(servo, random_position, duration, steps)
+    def generate_random_smooth_movement(self, servo):
+        min_limit = servo.getPhysicalLimitMin()
+        max_limit = servo.getPhysicalLimitMax()
 
-def perform_random_smooth_movements(servos, duration=1.0, steps=50):
-    while True:
-        # Seleccionar un servo aleatorio
-        random_servo = random.choice(servos)
+        # Generar una posición aleatoria dentro de los límites físicos del servo
+        random_position = random.randint(min_limit, max_limit)
 
-        # Generar y aplicar un movimiento suave aleatorio
-        generate_random_smooth_movement(random_servo, duration, steps)
+        # Mover suavemente el servo a la posición aleatoria
+        self.generate_smooth_movement(servo, random_position)
 
-        # Esperar un tiempo antes de realizar el próximo movimiento
-        time.sleep(random.uniform(0.5, 2.0))  # Puedes ajustar estos valores según tus necesidades
+    def perform_random_smooth_movements(self):
+        while True:
+            # Seleccionar un servo aleatorio
+            random_servo = random.choice(self.servos_data)
+
+            # Generar y aplicar un movimiento suave aleatorio
+            self.generate_random_smooth_movement(random_servo)
 
 if __name__ == "__main__":
-    kit = ServoKit(channels=16)  # Asegúrate de ajustar el número de canales según tu configuración
-    servos_data = skeleton_servos_data
-
-    initialize_servos(kit, servos_data)
+    # Crear una instancia de AnimatronicController
+    animatronic_controller = AnimatronicController(skeleton_servos_data)
 
     try:
-        perform_random_smooth_movements(servos_data)
+        # Iniciar movimientos suaves aleatorios
+        animatronic_controller.perform_random_smooth_movements()
     except KeyboardInterrupt:
         print("\nMovimientos aleatorios suaves detenidos.")
