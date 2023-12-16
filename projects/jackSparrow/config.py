6bed1641aa6513c35a7d795ba76c35f4a5e0019e
Jordi Orriols <info@jordiorriols.cat>
2023-12-16 14:14:44 +0100

    fix: add data to jack sparrow project

--- projects/jackSparrow/config.py
+++ projects/jackSparrow/config.py
@@ -1,3 +1,6 @@
 # Configuration
 
-jackSparrow_servos_data = []
+from common.config import mg996r_type
+from common.servo import AniServo
+
+jackSparrow_servos_data = [AniServo("body-flexion", 0, mg996r_type, 0, 180, 70)]
